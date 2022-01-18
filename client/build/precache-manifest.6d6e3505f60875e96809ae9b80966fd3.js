0c17d4623423cca849ff34286127331dcf38b505
Pratyush Kaware <pratyushkaware99@gmail.com>
2022-01-19 00:59:40 +0530

    Express and React

---
+++ client/build/precache-manifest.6d6e3505f60875e96809ae9b80966fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a3cfc66b5dd241efef126e99a52763",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31a5f6a0d17d606b8d64",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "10b61eb3fc61dfd2241d",
+    "url": "/static/js/3.80c7ac3e.chunk.js"
+  },
+  {
+    "revision": "31a5f6a0d17d606b8d64",
+    "url": "/static/js/main.24c56406.chunk.js"
+  },
+  {
+    "revision": "9984bfbb3bf8fb2f3a8b",
+    "url": "/static/js/runtime~main.51dbf2ec.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+var path = require('path')
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'client/build')));		
+app.get("/backendTest", (req, res) => res.send({data: 'Test'}));
+
+app.listen(4000)
