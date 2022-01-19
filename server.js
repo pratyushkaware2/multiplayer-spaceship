7124538869ab83b4f3ae42c6771730f35a815f84
pratyushkaware2 <pratyush.kaware@spit.ac.in>
2022-01-20 00:14:57 +0530

    initial commit

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 var cors = require('cors')
 var path = require('path')
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'client/build')));		
+// app.use(cors());
+
+// app.use(express.static(path.join(__dirname, 'client/build')));		
+
 app.get("/backendTest", (req, res) => res.send({data: 'Test'}));
 
 app.listen(process.env.PORT || 4000)
+
+var socketIO = require('socket.io')
+const io = socketIO(app);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
